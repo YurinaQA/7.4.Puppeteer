9575652ae7220cdcf43f67f85540fc9fec334dd7
YurinaQA <158807207+YurinaQA@users.noreply.github.com>
2024-10-21 23:26:42 +0500

    Тайм-ауты

--- gh.test.js
+++ gh.test.js
@@ -15,31 +15,27 @@ describe("Github page tests", () => {
 
   
   test("The h1 header content'", async () => {
-    jest.setTimeout(5000);
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
     expect(title2).toEqual(
-      "GitHub for teams · Build like the best teams on the planet · GitHub"
-    );
-  });
+      "GitHub for teams · Build like the best teams on the planet · GitHub");
+    }, 5000);
 
   test("The first link attribute", async () => {
-    jest.setTimeout(5000);
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 5000);
 
   test("The page contains Sign in button", async () => {
-    jest.setTimeout(5000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain("Get started with Team");
-  });
+  }, 5000);
 });
 
 
